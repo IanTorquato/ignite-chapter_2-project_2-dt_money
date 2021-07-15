63d9bf1987af50202f6e66a1f3ec6ef908d7663c
Ian Torquato <iantorquato2@gmail.com>
2021-07-15 20:15:56 -0300

    Loading default transactions

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,28 @@ createServer({
   models: {
     transaction: Model
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        { 
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Trabalho',
+          amount: 6000,
+          createdAt: new Date('2021-07-08 13:00:00')
+        },
+        { 
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1600,
+          createdAt: new Date('2021-07-10 16:00:00')
+        },
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api'
 
